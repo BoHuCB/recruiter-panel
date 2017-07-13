d5bb931bbc787c995cdbb66688b3cdfa88e60514
Bo Hu <Bo.Hu@careerbuilder.com>
2017-07-13 15:02:01 -0400

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderModule } from 'cb-vdl/src/app/header/header.module';
+import { FooterModule } from 'cb-vdl/src/app/footer/footer.module';
+import { CBFrameworkModule } from 'cb-vdl/src/app/cb-framework/cb-framework.module';
+
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './modules/main-page/main-page.component';
+import { appRoutes } from "./app.routing";
+import { RouterModule } from "@angular/router";
+
+const cbvdl = [
+  HeaderModule,
+  FooterModule,
+  CBFrameworkModule
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +22,9 @@ import { MainPageComponent } from './modules/main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ...cbvdl,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,4 @@
+import { Routes } from "@angular/router/router";
+
+export const appRoutes: Routes = [
+]
