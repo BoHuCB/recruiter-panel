5a06aebd1626f574b3ab82ec22426390d0fea7a5
Mauricio Ordonez <Mauricio.Ordonez@careerbuilder.com>
2017-07-13 16:10:16 -0400

    First changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './modules/main-page/main-page.component';
+import { StageComponent } from './modules/stage/stage.component';
+import { Ng2DragDropModule } from 'ng2-drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    StageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
